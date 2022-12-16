23d45673c3d2c43956a61afe885fc16712e12a1b
nuitfsdev <20521631@gm.uit.edu.vn>
2022-12-17 00:17:37 +0700

    add logout and update profile and test email

--- ShowroomCarIS220/Controllers/UserController.cs
+++ ShowroomCarIS220/Controllers/UserController.cs
@@ -7,7 +7,10 @@ using ShowroomCarIS220.Data;
 using ShowroomCarIS220.DTO.User;
 using ShowroomCarIS220.Models;
 using ShowroomCarIS220.Response;
+using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Net.Mail;
 using System.Security.Claims;
 using System.Text;
 
@@ -23,6 +26,120 @@ namespace ShowroomCarIS220.Controllers
         {
             _db = db;
             _config = config;
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("email")]
+        public void SendEmail()
+        {
+            try
+            {
+                string fromMail = "kingspeedmail@gmail.com";
+                string fromPassword = "ovprwckifgobfyeh";
+
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress(fromMail);
+                message.Subject = "Test Subject";
+                message.To.Add(new MailAddress("nuitfsdev@gmail.com"));
+                message.Body = "<html><body> Test Body </body></html>";
+                message.IsBodyHtml = true;
+
+                var smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587,
+                    Credentials = new NetworkCredential(fromMail, fromPassword),
+                    EnableSsl = true,
+                };
+
+                smtpClient.Send(message);
+                StatusCode(StatusCodes.Status200OK, "Success");
+
+            }
+            catch (Exception ex)
+            {
+                StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            
+        }
+        [HttpGet]
+        [Route("me")]
+        [Authorize()]
+        public async Task<ActionResult<UserResponse>> GetMyProfile()
+        {
+            try
+            {
+                var currentUser = GetCurrentUser();
+                if(currentUser == null)
+                {
+                    return StatusCode(StatusCodes.Status401Unauthorized,"Please authenticate");
+                }
+                return StatusCode(StatusCodes.Status200OK, currentUser);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        //Update User
+        [HttpPut]
+        [Route("me")]
+        [Authorize]
+        public async Task<ActionResult<GetUser>> UpdateUser([FromBody] UpdateUser userUpdate, [FromHeader] string Authorization)
+        {
+            try
+            {
+                var checkToken = CheckTokenLogout(Authorization.Substring(7));
+                if (checkToken == null)
+                {
+                    return StatusCode(StatusCodes.Status401Unauthorized, "Please authenticate");
+                }
+                var currentUser = GetCurrentUser();
+
+                if (currentUser == null)
+                {
+                    return StatusCode(StatusCodes.Status401Unauthorized, "Please authenticate");
+                }
+                var user = _db.User.FirstOrDefault(u => u.email == currentUser.email);
+                if(user == null)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, "User not exist");
+                }
+                if (userUpdate.name == null || userUpdate.name=="")
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "Name of User must have a value");
+
+                }
+                user.name = userUpdate.name;
+                user.gioitinh = userUpdate.gioitinh;
+                user.ngaysinh= userUpdate.ngaysinh;
+                user.sdt= userUpdate.sdt;
+                userUpdate.diachi = userUpdate.diachi;
+                userUpdate.chucvu = user.chucvu;
+                await _db.SaveChangesAsync();
+                var responeUser = new GetUser
+                {
+                    id = user.id,
+                    name = user.name,
+                    email = user.email,
+                    mauser = user.mauser,
+                    gioitinh = user.gioitinh,
+                    diachi = user.diachi,
+                    ngaysinh = user.ngaysinh,
+                    sdt = user.sdt,
+                    chucvu = user.chucvu,
+                    createdAt = user.createdAt,
+                    updatedAt = user.createdAt,
+                    role = user.role
+                };
+                return StatusCode(StatusCodes.Status200OK, responeUser);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+
+
         }
         //Sign up
         [AllowAnonymous]
@@ -106,7 +223,6 @@ namespace ShowroomCarIS220.Controllers
         public async Task<ActionResult<UserResponse>>  Login([FromBody] UserLogin userLogin)
         {
             var userAuth = Authenticate(userLogin);
-
             if (userAuth != null)
             {
                 var token = Generate(userAuth.email, userAuth.role);
@@ -127,6 +243,31 @@ namespace ShowroomCarIS220.Controllers
             }
 
             return StatusCode(StatusCodes.Status400BadRequest,"Error Email Or Password");
+        }
+        //Logout
+        [HttpPost]
+        [Route("logout")]
+        [Authorize]
+        public async Task<ActionResult<GetUser>> Logout([FromHeader] string Authorization)
+        {
+            try
+            {
+                var checkToken = CheckTokenLogout(Authorization.Substring(7));
+                if (checkToken == null)
+                {
+                    return StatusCode(StatusCodes.Status401Unauthorized, "Please authenticate");
+                }
+                var currentUser = GetCurrentUser();
+                
+
+                _db.Token.Remove(checkToken);
+                await _db.SaveChangesAsync();
+                return StatusCode(StatusCodes.Status200OK,"Ok");
+
+            }catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
         private string Generate(string email, string role)
         {
@@ -187,6 +328,49 @@ namespace ShowroomCarIS220.Controllers
                 return null;
             }
            
+        }
+        private GetUser? GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                var emailUser = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value;
+                var currentUser=_db.User.SingleOrDefault(u=>u.email==emailUser);
+                if(currentUser == null)
+                {
+                    return null;
+                }
+                return new GetUser
+                {
+                    id = currentUser.id,
+                    name = currentUser.name,
+                    email = currentUser.email,
+                    role = currentUser.role,
+                    mauser = currentUser.mauser,
+                    gioitinh = currentUser.gioitinh,
+                    ngaysinh = currentUser.ngaysinh,
+                    sdt = currentUser.sdt,
+                    diachi = currentUser.diachi,
+                    chucvu = currentUser.chucvu,
+                    createdAt = currentUser.createdAt,
+                    updatedAt = currentUser.updatedAt,
+
+                };
+            }
+            return null;
+        }
+       
+        private Token? CheckTokenLogout(string token)
+        {
+            var tokenUser = _db.Token.SingleOrDefault(t => t.token == token);
+
+            if (tokenUser == null)
+            {
+                return null;
+            }
+            return tokenUser;
         }
     }
 }

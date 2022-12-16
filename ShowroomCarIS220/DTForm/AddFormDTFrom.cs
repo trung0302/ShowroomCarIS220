3d429d0622f03223021acbf9f8a328b43dc8020a
TonNuThaoNhi <103103416+TonNuThaoNhi@users.noreply.github.com>
2022-12-16 19:40:32 +0700

    creat getForm, getFormById, addFrom, removeForm

---
+++ ShowroomCarIS220/DTForm/AddFormDTFrom.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ShowroomCarIS220.DTForm
+{
+    public class AddFormDTFrom
+    {
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string mobile { get; set; }
+        [Required]
+        public string email { get; set; }
+        public string message { get; set; }
+    }
+}

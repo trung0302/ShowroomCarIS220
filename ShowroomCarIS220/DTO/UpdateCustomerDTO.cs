ff1e23b44a4ac2566b9dc0cb5d73b56a85ee7029
dangbalinh <linha1xp@gmail.com>
2022-12-16 14:48:31 +0700

    customer controller

---
+++ ShowroomCarIS220/DTO/UpdateCustomerDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShowroomCarIS220.DTO
+{
+    public class UpdateCustomerDTO
+    {
+        [Required]
+        public string ten { get; set; }
+        [Required]
+        public string diachi { get; set; }
+        [Required]
+        public long cccd { get; set; }
+        [Required]
+        public long sodienthoai { get; set; }
+    }
+}

48ac82fecb6dfbd5df77227ca7390725d3771f5a
QuocBao1097 <20521097@gm.uit.edu.vn>
2022-12-19 14:59:12 +0700

    update api employees

--- ShowroomCarIS220/DTO/Employee/UpdateEmployeeDTO.cs
+++ ShowroomCarIS220/DTO/Employee/UpdateEmployeeDTO.cs
@@ -4,17 +4,10 @@ namespace ShowroomCarIS220.DTO.Employee
 {
     public class UpdateEmployeeDTO
     {
-        [Required]
         public string name { get; set; }
-        [Required]
         public string diachi { get; set; }
-        [Required]
         public string ngaysinh { get; set; }
-        [Required]
         public string chucvu { get; set; }
-        [Required]
-        public string sodienthoai { get; set; }
-        [Required]
-        public int cccd { get; set; }
+        public string sdt { get; set; }
     }
 }
--- ShowroomCarIS220/Program.cs
+++ ShowroomCarIS220/Program.cs
@@ -31,10 +31,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
 {
     //options.UseSqlServer(builder.Configuration.GetConnectionString("NamConnections"));
-    options.UseSqlServer(builder.Configuration.GetConnectionString("TrungConnections"));
+    //options.UseSqlServer(builder.Configuration.GetConnectionString("TrungConnections"));
     //options.UseSqlServer(builder.Configuration.GetConnectionString("NhiConnections"));
 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("BaoConnections"));
+    //options.UseSqlServer(builder.Configuration.GetConnectionString("BaoConnections"));
 });
 
 builder.Services.AddScoped<IEmailInvoiceService, EmailInvoiceService>();
